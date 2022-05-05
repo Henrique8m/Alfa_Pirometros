35316b116e680e921591bca6d3d72e06cb5c5947
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-05-05 11:13:12 -0300

    Starting com a imagem da Yggdrasil

--- src/main/java/com/hrodriguesdev/AlfaPirometrosApplication.java
+++ src/main/java/com/hrodriguesdev/AlfaPirometrosApplication.java
@@ -2,28 +2,45 @@ package com.hrodriguesdev;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-public class AlfaPirometrosApplication extends Application{
+public class AlfaPirometrosApplication extends Application {
 	private static Scene scene;
-	private static Stage stage;	
-	
+	private static Stage stage;
+	private ImageView starting;
+	private Image icon;
+
+	private final String nameIcon = "Yggdrasil icon.jpg";
+	private final String nameImageViewStarting = "Yggdrasil.jpg";
+
 	@Override
 	public void start(Stage arg0) throws Exception {
 
+		loadImage(nameImageViewStarting);
 		stage = arg0;
 		Pane pane = new Pane();
+		pane.getChildren().add(starting);
 		scene = new Scene(pane, 400, 300);
-		stage = new Stage();		
+		stage = new Stage();
+
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.setScene(scene);
 		stage.setTitle("Contagem Caixotes");
+		stage.getIcons().add(icon);
 		stage.show();
-		
+
 	}
-	
+
+	private void loadImage(String nameImageViewStarting) {
+		starting = new ImageView(new Image(
+				AlfaPirometrosApplication.class.getResource("gui/resources/" + nameImageViewStarting).toString()));
+		icon = new Image(AlfaPirometrosApplication.class.getResource("gui/resources/" + nameIcon).toString());
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
