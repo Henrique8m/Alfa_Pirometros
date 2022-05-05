1195784955c85adb8550bc1eae1d7df88f94e42a
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-05-05 10:53:57 -0300

    Projeto criado

---
+++ src/main/java/com/hrodriguesdev/AlfaPirometrosApplication.java
@@ -0,0 +1,13 @@
+package com.hrodriguesdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfaPirometrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlfaPirometrosApplication.class, args);
+	}
+
+}
